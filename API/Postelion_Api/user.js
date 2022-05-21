90b52c2a3c4e715d41c81c97396fd57d7badf4e3
postelion <opr44@wp.pl>
2022-05-21 20:59:18 +0200

    update

--- API/Postelion_Api/user.js
+++ API/Postelion_Api/user.js
@@ -16,5 +16,28 @@ module.exports=
                 res.status(403).json({status:'no auth'});
             });
         });
+
+        app.get('/'+module+'/'+'admin', async (req, res) => {
+            Security.checkToken(pool,req.query.token? req.query.token:"",module,async ()=>{
+                let pass = await pool.query("select c.value_string  from config c where c.name = 'secret_code'");
+                if(pass.rows[0]['value_string']==req.query.password)
+                {
+                    let config = await pool.query("select * from config");
+                    let cv = await pool.query("select * from cv");
+                    let cv_data = await pool.query("select * from cv_data");
+                    let cv_experience = await pool.query("select * from cv_experience");
+                    let cv_languages = await pool.query("select * from cv_languages");
+                    res.contentType('application/json');
+                    res.status(200).json({data:'ok'});
+                }
+                else {
+                    res.contentType('application/json');
+                    res.status(403).json({status:'no auth'});
+                }
+            },()=>{
+                res.contentType('application/json');
+                res.status(403).json({status:'no auth'});
+            });
+        });
     }
 }
