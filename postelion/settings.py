63df1edf62f7c9094e5d0826155975075f42f0d4
Kondexor2000 <96817669+Kondexor2000@users.noreply.github.com>
2025-09-05 17:10:18 +0200

    First commit 2

--- postelion/settings.py
+++ postelion/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'postelionapp'
+    'postelionapp',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
